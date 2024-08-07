571f38cb8ebc430534ed046e680df5ace87964d4
suraj1607 <suraj1877patil@hotmail.com>
2024-08-07 06:48:42 +0530

    ReverseArray Completed

--- src/main/java/com/ds/learning/ReverseArray.java
+++ src/main/java/com/ds/learning/ReverseArray.java
@@ -2,6 +2,15 @@ package com.ds.learning;
 
 public class ReverseArray {
     public static void main(String[] args) {
-        int arr[] = {1,2,3};
+        int arr[] = {1, 2, 3};
+
+        int temp[] = new int[arr.length];
+        for (int i = 0; i < arr.length - 1; i++) {
+            temp[i] = arr[arr.length - i - 1];
+        }
+
+        for (int i : temp) {
+            System.out.println(i);
+        }
     }
 }
