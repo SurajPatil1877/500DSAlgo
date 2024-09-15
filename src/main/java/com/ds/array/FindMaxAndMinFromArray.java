2c1a51540fa91410c8b25521fd30574be164147d
suraj1607 <suraj1877patil@hotmail.com>
2024-09-15 15:49:44 +0530

    Find Max and Min from array

--- src/main/java/com/ds/array/FindMaxAndMinFromArray.java
+++ src/main/java/com/ds/array/FindMaxAndMinFromArray.java
@@ -1,8 +1,6 @@
 package com.ds.array;
 
 import java.util.Arrays;
-import java.util.OptionalInt;
-import java.util.stream.IntStream;
 
 public class FindMaxAndMinFromArray {
     public static void main(String[] args) {
---
+++ src/main/java/com/practice/StudentMain.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class StudentMain {
+
+    public static void main(String[] args) {
+        StudentRecord student = new StudentRecord();
+
+
+    }
+}
